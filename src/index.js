c1cc7e2e61ea0e6fe521c9523266f74f46d4afd5
BMike <cypher3204@gmail.com>
2024-02-16 00:03:49 +0700

    Страница login

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import errorPage from "./pages/errorPage/errorPage.hbs";
+import Handlebars from "handlebars/runtime";
+// страницы
+import errorPage from "./pages/error/error.hbs";
+import loginPage from "./pages/login/login.hbs";
+import button from "./components/button/button.hbs";
+// стили
+import buttonStyles from "./components/button/button.css";
+import loginStyle from "./pages/login/login.css"
+
+Handlebars.registerPartial("button", button);
 
 document.addEventListener("DOMContentLoaded", () => {
     const app = document.querySelector("#app");
     const path = window.location.pathname;
-    console.log("PATH", path)
     switch (path) {
         case "/":
         case "":
             app.innerHTML = "Start page";
+            break
+        case "/login":
+            app.innerHTML = loginPage();
             break
         case "/page404":
             app.innerHTML = errorPage({ message: "Не найдено"});
