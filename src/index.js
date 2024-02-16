40fd5ee516c79754998bfeb40d3ec86f1280b784
BMike <cypher3204@gmail.com>
2024-02-16 22:04:58 +0700

    страница регистрации

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import Handlebars from "handlebars/runtime";
 // страницы
 import errorPage from "./pages/error/error.hbs";
 import loginPage from "./pages/login/login.hbs";
-import button from "./components/button/button.hbs";
+import registerPage from "./pages/register/register.hbs";
+import button from "./common/components/button/button.hbs";
 // стили
-import buttonStyles from "./components/button/button.css";
+import buttonStyles from "./common/components/button/button.css";
+import styles from "./common/styles/styles.css";
 import loginStyle from "./pages/login/login.css"
 
 Handlebars.registerPartial("button", button);
@@ -19,6 +21,9 @@ document.addEventListener("DOMContentLoaded", () => {
             break
         case "/login":
             app.innerHTML = loginPage();
+            break
+        case "/register":
+            app.innerHTML = registerPage();
             break
         case "/page404":
             app.innerHTML = errorPage({ message: "Не найдено"});
