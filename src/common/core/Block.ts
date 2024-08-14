aa8655c15cf54c1af932485b2a926f5680d96067
BMike <cypher3204@gmail.com>
2024-08-14 14:46:48 +0700

    docs

--- src/common/core/Block.ts
+++ src/common/core/Block.ts
@@ -1,7 +1,4 @@
-// import { v4 } from 'uuid';
 import Handlebars from 'handlebars';
-// import EventBus from './EventBus';
-// import { State } from './Store';
 
 import chat from '../components/chat/chat.hbs';
 import message from '../components/message/message.hbs';
@@ -9,12 +6,15 @@ import { default as dataRow } from '../components/dataRow/dataRow.hbs';
 import { default as button } from '../components/button/button.hbs';
 import { default as menu } from '../svg/menu.hbs';
 import { default as attach } from '../svg/attach.hbs';
-// import { isEqual } from '../utils/objectUtils';
 import BlockBase from './BlockBase';
 
 /*
 *   Приходится регистрировать partials здесь
 *   Так как в противном случае они будут недоступны для шаблонов страниц, использующихся в блоках
+*
+*   Отделено от BlockBase класса, так как при импорте в файлах теста возникала ошибка из-за невозможности импортировать
+*   .hbs файлы в Node окружении.
+*
 * */
 Handlebars.registerPartial('chat', chat);
 Handlebars.registerPartial('menu', menu);
